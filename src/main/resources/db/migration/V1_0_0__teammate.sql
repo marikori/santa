74de920b02b9ffc9a609d8af32f7b3a68145b7fe
marikori <marikori@ie.ibm.com>
2021-12-17 20:29:48 +0000

    db scripts

---
+++ src/main/resources/db/migration/V1_0_0__teammate.sql
@@ -0,0 +1,28 @@
+CREATE TABLE IF NOT EXISTS team_mate (
+    -- This is an auto incrementing bigint.
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR,
+    current_santa VARCHAR,
+   	one_year_back_santa VARCHAR,
+    two_years_back_santa VARCHAR,
+    three_years_back_santa VARCHAR
+);
+
+ALTER TABLE team_mate
+    ADD CONSTRAINT team_mate_unique UNIQUE (name);
+
+ALTER TABLE team_mate
+    ADD CONSTRAINT team_mate_current_fk FOREIGN KEY (current_santa) 
+        REFERENCES team_mate;
+
+ALTER TABLE team_mate
+    ADD CONSTRAINT team_mate_one_fk FOREIGN KEY (one_year_back_santa) 
+        REFERENCES team_mate;
+
+ALTER TABLE team_mate
+    ADD CONSTRAINT team_mate_two_fk FOREIGN KEY (two_years_back_santa) 
+        REFERENCES team_mate;
+
+ALTER TABLE team_mate
+    ADD CONSTRAINT team_mate_three_fk FOREIGN KEY (three_years_back_santa) 
+        REFERENCES team_mate;
