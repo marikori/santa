8a667a74d7c4a03a1c527a20358db566a505ae7e
marikori <marikori@ie.ibm.com>
2021-12-17 16:04:59 +0000

    init project

---
+++ src/test/java/com/secret/santa/SantaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secret.santa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
